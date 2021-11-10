7ddc535efc459946eb2f3f224c6cb29f1ad24905
EstebanJPG <esteban.jimenez.piquerass@gmail.com>
2021-11-10 19:15:00 +0100

    dtdrg
    fdg

---
+++ app/src/main/java/dam/esteban/u3t3menuofactivitis/Activity3.java
@@ -0,0 +1,14 @@
+package dam.esteban.u3t3menuofactivitis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+    }
+}
---
+++ app/src/main/java/dam/esteban/u3t3menuofactivitis/MainActivity.java
@@ -0,0 +1,46 @@
+package dam.esteban.u3t3menuofactivitis;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements  MyAdapter.OnItemClickListener {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private String [] myDataSet= {"Activity1", "Activity2", "Activity3"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUI();
+    }
+
+    private void setUI(){
+        recyclerView = findViewById(R.id.recyclerViewActivitis);
+        //Bloqueamos el tamaño
+        recyclerView.setHasFixedSize(true);
+
+        //Añadimos al recycler un layout
+
+        layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        //Añadimos el adapatador al recycler
+
+        mAdapter = new MyAdapter(myDataSet, this);
+        recyclerView.setAdapter(mAdapter);
+
+    }
+
+    @Override
+    public void onItemClick(String activityName) {
+        Toast.makeText(this, activityName, Toast.LENGTH_SHORT).show();
+    }
+}
